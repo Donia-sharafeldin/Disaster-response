5262ea1dba2a3b1da15db5596c6416539b7b9423
Donia-sharafeldin <donia.199887@gmail.com>
2022-01-20 01:01:36 +0200

    first

---
+++ app/app.py
@@ -0,0 +1,145 @@
+import json
+import plotly, pickle
+import pandas as pd
+import numpy as np
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request
+from plotly.graph_objs import Bar
+
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+
+
+def tokenize(text):
+    words = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    lem = []
+    for w in words:
+        word = lemmatizer.lemmatize(w).lower().strip() 
+        lem.append(word)
+    return lem
+
+
+engine = create_engine('sqlite:///../data//dis.db')
+df = pd.read_sql_table('last', engine)
+
+model = pickle.load(open('../models/mod.pkl','rb'))
+
+@app.route('/')
+@app.route('/home')
+def index():
+    
+    # extract data needed for visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # Show distribution of different category
+    category = list(df.columns[4:])
+    category_counts = []
+    for column_name in category:
+        category_counts.append(np.sum(df[column_name]))
+
+    # extract data exclude related
+    categories = df.iloc[:,4:]
+    categories_mean = categories.mean().sort_values(ascending=False)[1:11]
+    categories_names = list(categories_mean.index)
+
+
+    # create visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=categories_names,
+                    y=categories_mean
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 10 Message Categories',
+                'yaxis': {
+                    'title': "Percentage"
+                },
+                'xaxis': {
+                    'title': "Categories"
+                }
+            }
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('home.html', ids=ids, graphJSON=graphJSON)
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/results')
+def predict():
+    #  save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'results.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+@app.route('/contact')
+def contact():
+        return render_template('contactus.html')
+
+  
+if __name__ == "__main__":
+    app.run(debug=True)
